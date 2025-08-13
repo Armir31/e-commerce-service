b1f81d144e1d006e3399cf1237b6886710540d6e
armirigj <armirgjini76@gmail.com>
2025-08-13 19:47:05 +0200

    First steps for service classes

--- src/main/java/al/vibe/nile/dto/CategoryDto.java
+++ src/main/java/al/vibe/nile/dto/CategoryDto.java
@@ -1,4 +1,11 @@
 package al.vibe.nile.dto;
 
+import al.vibe.nile.entity.Product;
+import lombok.Data;
+
+@Data
 public class CategoryDto {
+    private String name;
+    private String description;
+    private Product product;
 }
--- src/main/java/al/vibe/nile/dto/ProductDto.java
+++ src/main/java/al/vibe/nile/dto/ProductDto.java
@@ -1,4 +1,12 @@
 package al.vibe.nile.dto;
 
+import lombok.Data;
+
+@Data
 public class ProductDto {
+    private String name;
+    private String description;
+    private String image;
+    private String price;
+    private Integer quantity;
 }
