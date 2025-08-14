d6d2e322c0a2b5a56d2c73c0c7f91e624fb0888d
armirigj <armirgjini76@gmail.com>
2025-08-14 21:52:31 +0200

    First steps for the classes needed for Payments

--- src/main/java/al/vibe/nile/repository/PaymentRepository.java
+++ src/main/java/al/vibe/nile/repository/PaymentRepository.java
@@ -1,4 +1,9 @@
 package al.vibe.nile.repository;
 
-public interface PaymentRepository {
+import al.vibe.nile.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
 }
